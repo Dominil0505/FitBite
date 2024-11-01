e6908307ea2c8851a87e085460b8424956729305
Dominik <dominik.szabo.03@gmail.com>
2024-11-01 16:57:20 +0100

    Allergy Page

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,5 @@
+using BaseLibrary.DTOs;
+using BaseLibrary.Entities;
 using Blazored.LocalStorage;
 using Client;
 using ClientLibrary.Helpers;
@@ -12,29 +14,31 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
+builder.Services.AddTransient<CustomHttpHandler>();
 builder.Services.AddHttpClient("SystemApiClient", client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7046");
-});
+    client.BaseAddress = new Uri("https://localhost:7244");
+}).AddHttpMessageHandler<CustomHttpHandler>();
+
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<GetHttpClient>();
 builder.Services.AddScoped<LocalStorageService>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<IUserAccountService, UserAccountService>();
+
+// Roles for registration page
+builder.Services.AddScoped<IUserRoles<Roles>, UserRolesService<Roles>>();
+
+// Allergy | Disease | Ingredient | Medication
+builder.Services.AddScoped<IGenericServiceInterface<AllergyDTO>, GenericServiceImplementation<AllergyDTO>>();
+builder.Services.AddScoped<IGenericServiceInterface<Diseases>, GenericServiceImplementation<Diseases>>();
+builder.Services.AddScoped<IGenericServiceInterface<Ingredient>, GenericServiceImplementation<Ingredient>>();
+builder.Services.AddScoped<IGenericServiceInterface<Medication>, GenericServiceImplementation<Medication>>();
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-
-
-
-//builder.Services.AddOidcAuthentication(options =>
-//{
-//    // Configure your authentication provider options here.
-//    // For more information, see https://aka.ms/blazor-standalone-auth
-//    builder.Configuration.Bind("Local", options.ProviderOptions);
-//});
-
 
 
 await builder.Build().RunAsync();
